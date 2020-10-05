4589e5a2877192927bef98f529107eda6b6b7ded
Sarah <sarahborretti@gmail.com>
2020-10-05 12:20:06 -0300

    UNO 2 - bot consegue jogar depois do jogador

--- uno/uno.py
+++ uno/uno.py
@@ -2,6 +2,10 @@
 #------------
 import random
 
+#VARIAVEIS INICIAIS
+#-------------------
+baralho = ["+4 coringa", "+4 coringa", "+4 coringa", "+4 coringa", "1 vermelho", "2 vermelho", "3 vermelho", "4 vermelho", "5 vermelho", "6 vermelho", "7 vermelho", "8 vermelho", "9 vermelho", "1 amarelo", "2 amarelo", "3 amarelo", "4 amarelo", "5 amarelo", "6 amarelo", "7 amarelo", "8 amarelo", "9 amarelo",]
+
 #FUNÇÕES
 #---------
 def escolhe_carta_mesa(descarte):
@@ -9,10 +13,9 @@ def escolhe_carta_mesa(descarte):
     while i == 0: #enquanto inicializador for 0
         for p in range(len(descarte)):
             carta_atual = descarte[p].split() #pega a carta de índice p e separa em duas (número / cor)
-            print(carta_atual)
                     
             if carta_atual[0] != "+4": #se a carta não for especial
-                return descarte[i] #retorna a carta atual
+                return descarte[p] #retorna a carta atual
                 i = 1
 
 def carta_possivel(mao, indice, carta_mesa):
@@ -27,7 +30,7 @@ def carta_possivel(mao, indice, carta_mesa):
 
 def vez_jogador(cartas):
     carta = "errada" #carta inicialmente sendo errada
-    print("\nSUA VEZ DE JOGAR, escolha sua carta de acordo com o índice (começando do 0): ")
+    print("\nSUA VEZ DE JOGAR, escolha sua carta de acordo com o índice (começando do 0):")
     print(cartas)
     escolha = int(input("")) #jogador escolhe o índice da carta
 
@@ -39,18 +42,23 @@ def vez_jogador(cartas):
             print(cartas)
             escolha = int(input("")) #jogador escolhe outra
         else:
-            print("Essa carta pode ser escolhida")
+            #print("Essa carta pode ser escolhida")
             carta = "certa"
-            return carta_certa #retorna a carta que ele escolheu
+            print("VOCÊ ESCOLHEU", cartas[escolha])
+            return cartas[escolha] #retorna a carta que ele escolheu
 
-#def vez_bot():
+def vez_bot(cartas):
+    print("\nVEZ DA CPU")
+    print(cartas)
+    
+    for i in range(len(cartas)): #corre o baralho do bot (primeira carta possível será a escolhida)
+        carta_certa = carta_possivel(cartas, i, carta_mesa) #executa a função pra ver se a carta é certa
 
-#VARIAVEIS INICIAIS
-#-------------------
-baralho = ["+4", "+4", "+4", "+4", "1 vermelho", "2 vermelho", "3 vermelho", "4 vermelho", "5 vermelho", "6 vermelho", "7 vermelho", "8 vermelho", "9 vermelho", "1 amarelo", "2 amarelo", "3 amarelo", "4 amarelo", "5 amarelo", "6 amarelo", "7 amarelo", "8 amarelo", "9 amarelo",]
+        if carta_certa == True: #se a carta for possível de ser jogada
+            print("O CPU ESCOLHEU", cartas[i])
+            return cartas[i] #retorna a carta escolhida
 
-
-#=============================================================================
+#===================================================================================================
 
 
 #MAIN
@@ -59,20 +67,22 @@ random.shuffle(baralho) #embaralha as cartas
 
 n = int(input("numero de cartas pra cada jogador: "))
 
-#DISTRIBUIÇÂO DAS CARTAS
+#---DISTRIBUIÇÂO DAS CARTAS---
 cartas_jog = baralho[0:n] #primeiras n cartas separadas pro jog
 cartas_bot = baralho[n:n*2] #as próximas n cartas pro bot
 descarte = baralho[n*2:-1] #restante das cartas pro descarte
 
-print("JOGADOR: ", cartas_jog)
-print("BOT: ", cartas_bot)
-print("DESCARTE: ", descarte)
+print("JOGADOR:", cartas_jog)
+print("BOT:", cartas_bot)
+print("DESCARTE:", descarte)
+
+#--ESCOLHE A CARTA DA MESA
 
 carta_mesa = escolhe_carta_mesa(descarte) #escolhe a carta da mesa
 
-print("\nCARTA DA MESA: ", carta_mesa)
+print("\nCARTA DA MESA:", carta_mesa)
 
-#EXCLUI A CARTA DA MESA DO DESCARTE
+#---EXCLUI A CARTA DA MESA DO DESCARTE---
 i = 0 #inicializador pra teste
 while i == 0: #enquanto o inicializador for0
     if descarte[i] == carta_mesa: #se a carta atual for a mesma da mesa
@@ -81,25 +91,22 @@ while i == 0: #enquanto o inicializador for0
 
 #print("DESCARTE: ", descarte)
 
-vez_jogador(cartas_jog)
+#---VEZ DO JOGADOR---
+carta_mesa = vez_jogador(cartas_jog) #carta da mesa vira a que foi escolhida pelo jog
 
+for i in range(len(cartas_jog)-1):
+    if cartas_jog[i] == carta_mesa: #se a carta atual for a mesma que ele escolheu
+        cartas_jog.pop(i) #exclui carta escolhida pra mesa do baralho do jog
 
+print("\nSEU BARALHO ATUALIZADO:", cartas_jog)
+print("\nNOVA CARTA DA MESA:", carta_mesa)
 
+#---VEZ DO BOT---
+carta_mesa = vez_bot(cartas_bot)
 
+for i in range(len(cartas_bot)-1):
+    if cartas_bot[i] == carta_mesa: #se a carta atual for a mesma que o bot escolheu
+        cartas_bot.pop(i) #exclui carta escolhida pra mesa do baralho do bot
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("\nBARALHO ATUALIZADO DO BOT:", cartas_jog)
+print("\nNOVA CARTA DA MESA:", carta_mesa)
